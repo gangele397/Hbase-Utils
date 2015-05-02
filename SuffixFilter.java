f62078aa0da23fc0fcd9e87f57561fbd57b63ee5
gangele397 <gangele397@gmail.com>
2015-05-02 14:01:41 +0530

    Suffix filter
    Support the Hbase scan from last

---
+++ SuffixFilter.java
@@ -0,0 +1,43 @@
+
+import java.io.IOException;
+
+import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.exceptions.DeserializationException;
+import org.apache.hadoop.hbase.filter.FilterBase;
+import org.apache.hadoop.hbase.util.Bytes;
+
+ 
+public class SuffixFilter extends FilterBase {
+    protected byte[] suffix = null;
+
+    public SuffixFilter(final byte[] suffix) {
+        this.suffix = suffix;
+    }
+    
+    @Override
+    public byte[] toByteArray() throws IOException {
+        return suffix;
+    }
+
+    @Override
+    public ReturnCode filterKeyValue(Cell ignored) throws IOException {
+      return ReturnCode.INCLUDE;
+    }
+    
+    @Override
+    public boolean filterRowKey(byte[] buffer, int offset, int length) {
+        if (buffer == null || this.suffix == null) return true;
+        if (length < suffix.length) return true;
+        // if they are equal, return false => pass row
+        // else return true, filter row
+        // if we are passed the suffix, set flag
+        int cmp = Bytes.compareTo(buffer, offset + (length - this.suffix.length),
+                this.suffix.length, this.suffix, 0, this.suffix.length);
+        return cmp != 0;
+    }
+
+    
+    public static SuffixFilter parseFrom(final byte [] pbBytes) throws DeserializationException {
+        return new SuffixFilter(pbBytes);
+    }
+}
